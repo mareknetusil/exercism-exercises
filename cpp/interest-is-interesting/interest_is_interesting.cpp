65bfb26d5994847fa3456fe1154e9c1d4d153dc2
netusil <m.netusil@mikrovox.cz>
2025-05-16 11:10:24 +0200

    interest-is-interesting; improved

--- cpp/interest-is-interesting/interest_is_interesting.cpp
+++ cpp/interest-is-interesting/interest_is_interesting.cpp
@@ -1,31 +1,34 @@
 #include <vector>
 #include <utility>
 #include <algorithm>
+#include <limits>
 
 // interest_rate returns the interest rate for the provided balance.
 double interest_rate(const double balance) {
-    const std::vector<std::pair<int, double>> rates = {
+    const std::vector<std::pair<double, double>> rates = {
         {0    , 3.213},
         {1'000, 0.5},
         {5'000, 1.621},
+        {std::numeric_limits<double>::max(), 2.475}
     };
-    const auto highest_rate = 2.475;
     const auto it = std::find_if(
         std::begin(rates), std::end(rates),
         [&balance](const auto threshold) { return balance < threshold.first ; }
         );
-    return it == std::end(rates) ? highest_rate : it->second;
+    return it->second;
 }
+
+constexpr auto PERCENT_DIVISOR = 100.;
 
 // yearly_interest calculates the yearly interest for the provided balance.
 double yearly_interest(const double balance) {
-    return balance * interest_rate(balance) / 100.0;
+    return balance * interest_rate(balance) / PERCENT_DIVISOR;
 }
 
 // annual_balance_update calculates the annual balance update, taking into
 // account the interest rate.
 double annual_balance_update(const double balance) {
-    return balance * (1 + interest_rate(balance) / 100.);
+    return balance * (1 + interest_rate(balance) / PERCENT_DIVISOR);
 }
 
 // years_until_desired_balance calculates the minimum number of years required
