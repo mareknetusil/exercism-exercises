74dc4fef0eefb8f7dd8b8c60484e3d4c05435f37
netusil <m.netusil@mikrovox.cz>
2025-07-21 11:56:45 +0200

    cpp: luhn solution update

--- cpp/luhn/luhn.cpp
+++ cpp/luhn/luhn.cpp
@@ -6,26 +6,27 @@ namespace luhn {
 bool valid(std::string_view text) {
     int sum = 0;
     size_t count = 0;
-    bool double_ = false;
+    bool should_double = false;
     for (auto cIt = std::rbegin(text); cIt != std::rend(text); ++cIt) {
         const auto c = *cIt;
-        if (c < '0' || '9' < c) {
-            if (c != ' ') {
-                return false;
-            }
+        if (c == ' ') {
             continue;
+        }
+
+        if (c < '0' || '9' < c) {
+            return false;
         }
 
         ++count;
         auto num = c - '0';
-        if (double_) {
+        if (should_double) {
             num *= 2;
             if (num > 9) {
                 num -= 9;
             }
         }
         sum += num;
-        double_ = !double_;
+        should_double = !should_double;
     }
     return count > 1 && sum % 10 == 0;
 }
