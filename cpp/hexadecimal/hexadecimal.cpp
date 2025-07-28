791be2520dfd559eca8b5bd91ef84ff9827b645a
netusil <m.netusil@mikrovox.cz>
2025-07-28 11:13:01 +0200

    cpp: hexadecimal update

--- cpp/hexadecimal/hexadecimal.cpp
+++ cpp/hexadecimal/hexadecimal.cpp
@@ -1,30 +1,4 @@
 #include "hexadecimal.h"
-#include <iterator>
-#include <stdexcept>
 
 namespace hexadecimal {
-
-int convert(std::string_view text) {
-    int retval = 0;
-    for (const auto c : text) {
-        // DIGIT
-        if ('0' <= c && c <= '9') {
-            retval = retval * 16 + c - '0';
-            continue;
-        }
-
-        // LETTER
-        const auto base = c >= 'a' ? 'a' : 'A';
-        const auto val = 10 + c - base;
-        if (10 <= val && val <=15) {
-            retval = retval * 16 + val;
-            continue;
-        }
-
-        // INVALID CHARACTER
-        return 0;
-    }
-    return retval;
-}
-
 }  // namespace hexadecimal
--- cpp/hexadecimal/hexadecimal.h
+++ cpp/hexadecimal/hexadecimal.h
@@ -1,8 +1,44 @@
 #pragma once
 
 #include <string_view>
+#include <climits>
+#include <optional>
+
 namespace hexadecimal {
 
-[[nodiscard]] int convert(std::string_view text);
+[[nodiscard]] constexpr long long convert(std::string_view text);
+
+constexpr std::optional<int> char_to_number(char c) {
+    // DIGIT
+    if ('0' <= c && c <= '9') {
+        return c - '0';
+    }
+
+    // LETTER
+    const auto base = c >= 'a' ? 'a' : 'A';
+    const auto val = 10 + c - base;
+    if (10 <= val && val <= 15) {
+        return val;
+    }
+
+    return std::nullopt;
+}
+
+constexpr long long convert(std::string_view text) {
+    long long retval = 0;
+    for (const auto c : text) {
+        const auto number = char_to_number(c);
+        if (!number) return 0;
+
+        // OVERFLOW CHECK
+        if (retval > (LLONG_MAX - *number) / 16) {
+            return 0;
+        }
+
+        retval = retval * 16 + *number;
+    }
+
+    return retval;
+}
 
 }  // namespace hexadecimal
