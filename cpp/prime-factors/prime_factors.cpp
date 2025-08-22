e30142fae962f64788815ebfde82c5765ef54276
netusil <m.netusil@mikrovox.cz>
2025-08-22 10:44:34 +0200

    cpp: prime-factors improvement

--- cpp/prime-factors/prime_factors.cpp
+++ cpp/prime-factors/prime_factors.cpp
@@ -2,43 +2,25 @@
 
 namespace prime_factors {
 
-long long next_prime(const std::vector<long long> &primes) {
-    long long n = primes.back() == 2 ? primes.back() + 1 : primes.back() + 2;
-    while (true) {
-        bool is_prime = true;
-        for (auto prime : primes) {
-            if (prime * prime > n) {
-                break;
-            }
-            if (n % prime == 0) {
-                is_prime = false;
-                break;
-            }
-        }
-
-        if (is_prime) {
-            return n;
-        }
-        n += 2;
-    }
-}
-
 std::vector<long long> of(long long n) {
-    if (n < 2) {
-        return {};
+    std::vector<long long> factors;
+
+    while (n % 2 == 0) {
+        factors.push_back(2);
+        n /= 2;
     }
 
-    std::vector<long long> primes{2};
-    std::vector<long long> factors{};
-    while (n != 1) {
-        auto prime = primes.back();
-        if (n % prime == 0) {
-            n /= prime;
-            factors.push_back(prime);
-        } else {
-            primes.emplace_back(next_prime(primes));
+    for (long long d = 3; d * d <=n; d += 2) {
+        while (n % d == 0) {
+            factors.push_back(d);
+            n /= d;
         }
     }
+
+    if (n > 1) {
+        factors.push_back(n);
+    }
+
     return factors;
 }
 
